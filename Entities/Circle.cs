34a90dab6c73d91dc441d8f604a2e1890c63ca05
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-22 18:19:34 -0300

    created program

--- Entities/Circle.cs
+++ Entities/Circle.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace AtividadeAbstrata.Entities
 {
-    public class Circle
+    public class Circle : Shape
     {
-        
+        public double Radius { get; set; }
+        public Circle ()
+        {
+        }
+        public Circle(double radius, Color color) : base (color)
+        {
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using AtividadeAbstrata.Entities;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            List<Shape> list = new List<Shape>();
+            Console.Write("Enter the number of shapes: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                System.Console.WriteLine("Shape #" + i + " data: ");
+                Console.Write("Rectangle or Circle (r/c): ");
+                char letter = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red) ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+                if (letter == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    list.Add(new Rectangle (width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Circle(radius, color));
+                }
+            }
+            foreach (Shape shap in list)
+            {
+                Console.WriteLine("Radius: " + shap.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
